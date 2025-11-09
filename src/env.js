66e4e738e1b7210ded0340687e2ae9cb52e99784
SugamChaudharry <nextgenachieverss@gmail.com>
2025-11-09 09:06:33 +0530

    fixing: versel url

--- src/env.js
+++ src/env.js
@@ -40,10 +40,7 @@ export const env = createEnv({
     BETTER_AUTH_GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
-    NEXT_PUBLIC_APP_URL:
-      process.env.VERCEL_URL
-        ? `https://${process.env.VERCEL_URL}`
-        : process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
